7390a8030ca090a667acbd3379ff0eb7da105f59
kengnidoriane <dorianekengni@gmail.com>
2025-01-30 15:35:23 +0100

    add name of agengy on the header and function to clear search in js

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,6 +1,7 @@
+const resultDiv = document.getElementById('result');
+
 function searchDestination() {
     const input = document.getElementById('destinationInput').value.toLowerCase();
-    const resultDiv = document.getElementById('result');
     resultDiv.innerHTML = '';
 
     fetch('travel_recommendation_api.json')
@@ -26,33 +27,6 @@ function searchDestination() {
                 }
             });
 
-            // data.countries.forEach(country => {
-            //     if (country.name.toLowerCase().includes(input)) {
-            //         const resultCard = document.createElement('div');
-            //         resultCard.classList.add('card');
-            
-            //         const countryName = document.createElement('h2');
-            //         countryName.textContent = country.name;
-            //         resultCard.appendChild(countryName);
-            
-            //         country.cities.forEach(city => {
-            //             const cityName = document.createElement('h3');
-            //             cityName.textContent = city.name;
-            //             resultCard.appendChild(cityName);
-            
-            //             const cityImage = document.createElement('img');
-            //             cityImage.src = city.imageUrl;
-            //             cityImage.alt = city.name;
-            //             resultCard.appendChild(cityImage);
-            
-            //             const cityDescription = document.createElement('p');
-            //             cityDescription.textContent = city.description;
-            //             resultCard.appendChild(cityDescription);
-            //         });
-            
-            //         resultDiv.appendChild(resultCard);
-            //     }
-            // });
             // find in temples
             if('temples'.includes(input)) {
                 data.temples.forEach(temple => {
@@ -66,20 +40,6 @@ function searchDestination() {
                     resultDiv.appendChild(resultCard);
                 })
             }
-            // find with temple name
-            // data.temples.forEach(temple => {
-            //     if (temple.name.toLowerCase().includes(input)) {
-            //         found = true;
-            //         const resultCard =  document.createElement('div')
-            //         resultCard.classList.add('card');
-            //         resultCard.innerHTML += `<img src="${temple.imageUrl}" alt="${temple.name}">`;
-            //         resultCard.innerHTML += `<h4>${temple.name}</h4>`;
-            //         resultCard.innerHTML += `<p>${temple.description}</p>`;
-            //         resultCard.innerHTML += `<button>Visit</button>`;
-            //         resultDiv.appendChild(resultCard);
-            //     }
-            // });
-
             // find in beaches
             if('beaches'.includes(input)) {
                 data.beaches.forEach(beach => {
@@ -93,20 +53,6 @@ function searchDestination() {
                     resultDiv.appendChild(resultCard);
                 })
             }
-            // find with beach name
-            // data.beaches.forEach(beach => {
-            //     if (beach.name.toLowerCase().includes(input)) {
-            //         found = true;
-            //         const resultCard =  document.createElement('div')
-            //             resultCard.classList.add('card');
-            //         resultCard.innerHTML += `<img src="${beach.imageUrl}" alt="${beach.name}">`;
-            //         resultCard.innerHTML += `<h4>${beach.name}</h4>`;
-            //         resultCard.innerHTML += `<p>${beach.description}</p>`;
-            //         resultCard.innerHTML += `<button>Visit</button>`;
-            //         resultDiv.appendChild(resultCard);
-            //     }
-            // });
-
 
 
             if (!found) {
@@ -121,3 +67,14 @@ function searchDestination() {
 
 const btnSearch = document.getElementById('btnSearch');
 btnSearch.addEventListener('click', searchDestination);
+
+function clearSearch() {
+    let input = document.getElementById('destinationInput');
+    console.log('hi');
+    
+    resultDiv.innerHTML = '';
+    input = '';
+}
+
+const btnClear = document.getElementById('btnClear');
+btnClear.addEventListener('click', clearSearch)
