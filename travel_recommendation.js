0019a3f0bada0e9bea1a68e2d774d29fc6fc86ea
kengnidoriane <dorianekengni@gmail.com>
2025-01-30 15:16:47 +0100

    style search card result

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -8,19 +8,18 @@ function searchDestination() {
         .then(data => {
             let found = false;
             console.log(data);
-            
             // found with country name
             data.countries.forEach(country => {
                 if (country.name.toLowerCase().includes(input)) {
                     found = true;
-                    
                     // resultDiv.innerHTML += `<h2>${country.name}</h2>`;
                     country.cities.forEach(city => {
                         const resultCard =  document.createElement('div')
                         resultCard.classList.add('card');
-                        resultCard.innerHTML += `<h3>${city.name}</h3>`;
                         resultCard.innerHTML += `<img src="${city.imageUrl}" alt="${city.name}">`;
+                        resultCard.innerHTML += `<h4>${city.name}</h4>`;
                         resultCard.innerHTML += `<p>${city.description}</p>`;
+                        resultCard.innerHTML += `<button>Visit</button>`;
                         resultDiv.appendChild(resultCard)
 
                     });
@@ -61,24 +60,25 @@ function searchDestination() {
                     const resultCard =  document.createElement('div')
                     resultCard.classList.add('card');
                     resultCard.innerHTML += `<img src="${temple.imageUrl}" alt="${temple.name}">`;
-                    resultCard.innerHTML += `<h2>${temple.name}</h2>`;
+                    resultCard.innerHTML += `<h4>${temple.name}</h4>`;
                     resultCard.innerHTML += `<p>${temple.description}</p>`;
+                    resultCard.innerHTML += `<button>Visit</button>`;
                     resultDiv.appendChild(resultCard);
                 })
             }
             // find with temple name
-            data.temples.forEach(temple => {
-                if (temple.name.toLowerCase().includes(input)) {
-                    found = true;
-                    const resultCard =  document.createElement('div')
-                    resultCard.classList.add('card');
-                    resultCard.innerHTML += `<img src="${temple.imageUrl}" alt="${temple.name}">`;
-                    resultCard.innerHTML += `<h2>${temple.name}</h2>`;
-                    resultCard.innerHTML += `<p>${temple.description}</p>`;
-                    resultCard.innerHTML += `<button>Visit</button>`;
-                    resultDiv.appendChild(resultCard);
-                }
-            });
+            // data.temples.forEach(temple => {
+            //     if (temple.name.toLowerCase().includes(input)) {
+            //         found = true;
+            //         const resultCard =  document.createElement('div')
+            //         resultCard.classList.add('card');
+            //         resultCard.innerHTML += `<img src="${temple.imageUrl}" alt="${temple.name}">`;
+            //         resultCard.innerHTML += `<h4>${temple.name}</h4>`;
+            //         resultCard.innerHTML += `<p>${temple.description}</p>`;
+            //         resultCard.innerHTML += `<button>Visit</button>`;
+            //         resultDiv.appendChild(resultCard);
+            //     }
+            // });
 
             // find in beaches
             if('beaches'.includes(input)) {
@@ -87,25 +87,25 @@ function searchDestination() {
                     const resultCard =  document.createElement('div')
                         resultCard.classList.add('card');
                     resultCard.innerHTML += `<img src="${beach.imageUrl}" alt="${beach.name}">`;
-                    resultCard.innerHTML += `<h2>${beach.name}</h2>`;
+                    resultCard.innerHTML += `<h4>${beach.name}</h4>`;
                     resultCard.innerHTML += `<p>${beach.description}</p>`;
                     resultCard.innerHTML += `<button>Visit</button>`;
                     resultDiv.appendChild(resultCard);
                 })
             }
             // find with beach name
-            data.beaches.forEach(beach => {
-                if (beach.name.toLowerCase().includes(input)) {
-                    found = true;
-                    const resultCard =  document.createElement('div')
-                        resultCard.classList.add('card');
-                    resultCard.innerHTML += `<img src="${beach.imageUrl}" alt="${beach.name}">`;
-                    resultCard.innerHTML += `<h2>${beach.name}</h2>`;
-                    resultCard.innerHTML += `<p>${beach.description}</p>`;
-                    resultCard.innerHTML += `<button>Visit</button>`;
-                    resultDiv.appendChild(resultCard);
-                }
-            });
+            // data.beaches.forEach(beach => {
+            //     if (beach.name.toLowerCase().includes(input)) {
+            //         found = true;
+            //         const resultCard =  document.createElement('div')
+            //             resultCard.classList.add('card');
+            //         resultCard.innerHTML += `<img src="${beach.imageUrl}" alt="${beach.name}">`;
+            //         resultCard.innerHTML += `<h4>${beach.name}</h4>`;
+            //         resultCard.innerHTML += `<p>${beach.description}</p>`;
+            //         resultCard.innerHTML += `<button>Visit</button>`;
+            //         resultDiv.appendChild(resultCard);
+            //     }
+            // });
 
 
 
